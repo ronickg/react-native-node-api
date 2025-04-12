c8af32ba862a4df18527a5739607361a7fe2e8eb
Kræn Hansen <mail@kraenhansen.dk>
2025-04-12 14:40:37 +0200

    Pin eslint@9

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    files: [
+      "**/babel.config.js",
+      "**/metro.config.js",
+      "**/metro-config.js",
+      "**/react-native.config.js",
+      "packages/example-cpp-node-api-addon/*.js",
+      "packages/react-native-node-api-modules/bin/*.js"
+    ],
+    languageOptions: {
+      parserOptions: {
+        sourceType: "commonjs",
+      },
+      globals: {
+        ...globals.commonjs,
+      },
+    },
+    rules: {
+      // We're using CommonJS here for Node.js backwards compatibility
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+);
--- packages/react-native-node-api-modules/bin/react-native-node-api-modules.js
+++ packages/react-native-node-api-modules/bin/react-native-node-api-modules.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+
 const { run } = require('../dist/cli/index.js');
 run(process.argv);
