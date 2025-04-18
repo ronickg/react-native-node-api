c8f339d301ffd3e34b69c94cd46e8cb9bf6703ed
Kræn Hansen <kraen.hansen@mongodb.com>
2025-04-19 01:33:40 +0200

    Copy files from PoC

---
+++ packages/react-native-node-api-modules/cpp/AddonLoaders.hpp
@@ -0,0 +1,104 @@
+#pragma once
+#include <assert.h>
+
+#if defined(__APPLE__) || defined(__ANDROID__)
+#include <dlfcn.h>
+#include <stdio.h>
+
+struct PosixLoader {
+  using Module = void *;
+  using Symbol = void *;
+
+  static Module loadLibrary(const char *filePath) {
+    assert(NULL != filePath);
+    Module result = dlopen(filePath, RTLD_NOW | RTLD_LOCAL);
+    if (NULL == result) {
+      fprintf(stderr, "NapiHost: Failed to load library '%s': %s", filePath,
+              dlerror());
+    }
+    return result;
+  }
+
+  static Symbol getSymbol(Module library, const char *name) {
+    assert(NULL != library);
+    assert(NULL != name);
+    Symbol result = dlsym(library, name);
+    // if (NULL == result) {
+    //   NSLog(@"NapiHost: Cannot find '%s' symbol!", name);
+    // }
+    return result;
+  }
+
+  static void unloadLibrary(Module library) {
+    if (NULL != library) {
+      dlclose(library);
+    }
+  }
+};
+#endif
+
+#if defined(_WIN32)
+struct Win32Loader {
+  using Module = HMODULE;
+  using Symbol = void *;
+
+  static Module loadLibrary(const char *filePath) {
+    assert(NULL != filePath);
+    Module result = LoadLibrary(filePath);
+    if (NULL == result) {
+      // TODO: Handle the error case... call GetLastError() that gives us error
+      // code as DWORD
+    }
+    return result;
+  }
+
+  static Symbol getSymbol(Module library, const char *name) {
+    assert(NULL != library);
+    assert(NULL != name);
+    Symbol result = GetProcAddress(library, name);
+    if (NULL == result) {
+      // TODO: Handle the error case... call GetLastError() that gives us error
+      // code as DWORD
+    }
+    return result;
+  }
+
+  static void unloadLibrary(Module library) {
+    if (NULL != library) {
+      FreeLibrary(library);
+    }
+  }
+};
+
+struct WinRTLoader {
+  using Module = HMODULE;
+  using Symbol = void *;
+
+  static Module loadLibrary(const char *filePath) {
+    assert(NULL != filePath);
+    Module result = LoadPackagedLibrary(filePath);
+    if (NULL == result) {
+      // TODO: Handle the error case... call GetLastError() that gives us error
+      // code as DWORD
+    }
+    return result;
+  }
+
+  static Symbol getSymbol(Module library, const char *name) {
+    assert(NULL != library);
+    assert(NULL != name);
+    Symbol result = GetProcAddress(library, name);
+    if (NULL == result) {
+      // TODO: Handle the error case... call GetLastError() that gives us error
+      // code as DWORD
+    }
+    return result;
+  }
+
+  static void unloadLibrary(Module library) {
+    if (NULL != library) {
+      FreeLibrary(library);
+    }
+  }
+};
+#endif
---
+++ packages/react-native-node-api-modules/ios/NodeApiHostModuleProvider.mm
@@ -0,0 +1,41 @@
+#import "CxxNodeApiHostModule.hpp"
+
+#define USE_CXX_TURBO_MODULE_UTILS 0
+#if defined(__has_include)
+#if __has_include(<ReactCommon/CxxTurboModuleUtils.h>)
+#undef USE_CXX_TURBO_MODULE_UTILS
+#define USE_CXX_TURBO_MODULE_UTILS 1
+#endif
+#endif
+
+#if USE_CXX_TURBO_MODULE_UTILS
+#import <ReactCommon/CxxTurboModuleUtils.h>
+@interface NodeApiHost : NSObject
+#else
+#import <ReactCommon/RCTTurboModule.h>
+@interface NodeApiHost : NSObject <RCTBridgeModule, RCTTurboModule>
+#endif // USE_CXX_TURBO_MODULE_UTILS
+
+@end
+
+@implementation NodeApiHost
+#if USE_CXX_TURBO_MODULE_UTILS
++ (void)load {
+  facebook::react::registerCxxModuleToGlobalModuleMap(
+      callstack::nodeapihost::CxxNodeApiHostModule::kModuleName,
+      [](std::shared_ptr<facebook::react::CallInvoker> jsInvoker) {
+        return std::make_shared<callstack::nodeapihost::CxxNodeApiHostModule>(
+            jsInvoker);
+      });
+}
+#else
+RCT_EXPORT_MODULE()
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
+  return std::make_shared<callstack::nodeapihost::CxxNodeApiHostModule>(
+      params.jsInvoker);
+}
+#endif // USE_CXX_TURBO_MODULE_UTILS
+
+@end
