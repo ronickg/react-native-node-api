55d8a61b5b4f0ca2d49f1819c6af86c2f38b0766
Kræn Hansen <kraen.hansen@mongodb.com>
2025-04-24 13:54:49 +0200

    Failed experiment

--- packages/react-native-node-api-modules/src/node/cli/index.ts
+++ packages/react-native-node-api-modules/src/node/cli/index.ts
@@ -3,6 +3,7 @@ import path from "node:path";
 import fs from "node:fs";
 import cp from "node:child_process";
 import { createRequire } from "node:module";
+import os from "node:os";
 
 import { Command } from "@commander-js/extra-typings";
 
@@ -10,7 +11,13 @@ import { hashNodeApiModulePath } from "../path-utils.js";
 
 // Must be in all xcframeworks to be considered as Node-API modules
 const MAGIC_FILENAME = "react-native-node-api-module";
-const XCFRAMEWORKS_PATH = path.resolve(__dirname, "../../../xcframeworks");
+// const XCFRAMEWORKS_PATH = path.resolve(__dirname, "../../../xcframeworks");
+const XCFRAMEWORK_PATH = path.resolve(
+  __dirname,
+  "../../../node-api-modules.xcframework"
+);
+
+console.log(XCFRAMEWORK_PATH);
 
 /**
  * Search upwards from a directory to find a package.json and
@@ -59,19 +66,53 @@ export function findPackageDependencyPaths(
   }
 }
 
-export function findXCFrameworkPaths(dependencyPath: string): string[] {
+// export function findXCFrameworkPaths(dependencyPath: string): string[] {
+//   return fs
+//     .readdirSync(dependencyPath, { withFileTypes: true })
+//     .flatMap((file) => {
+//       if (
+//         file.isFile() &&
+//         file.name === MAGIC_FILENAME &&
+//         path.extname(dependencyPath) === ".xcframework"
+//       ) {
+//         return [dependencyPath];
+//       } else if (file.isDirectory()) {
+//         // Traverse into the child directory
+//         return findXCFrameworkPaths(path.join(dependencyPath, file.name));
+//       }
+//       return [];
+//     });
+// }
+
+export function findFrameworkPaths(currentPath: string): string[] {
   return fs
-    .readdirSync(dependencyPath, { withFileTypes: true })
+    .readdirSync(currentPath, { withFileTypes: true })
     .flatMap((file) => {
-      if (
-        file.isFile() &&
-        file.name === MAGIC_FILENAME &&
-        path.extname(dependencyPath) === ".xcframework"
-      ) {
-        return [dependencyPath];
-      } else if (file.isDirectory()) {
-        // Traverse into the child directory
-        return findXCFrameworkPaths(path.join(dependencyPath, file.name));
+      if (file.isDirectory()) {
+        if (path.extname(file.name) === ".xcframework") {
+          const magicFilePath = path.join(
+            currentPath,
+            file.name,
+            MAGIC_FILENAME
+          );
+          if (fs.existsSync(magicFilePath)) {
+            // We've found an xcframework intended for Node-API linking
+            return fs
+              .readdirSync(path.join(currentPath, file.name), {
+                withFileTypes: true,
+                recursive: true,
+              })
+              .filter(
+                (entry) =>
+                  entry.isDirectory() &&
+                  path.extname(entry.name) === ".framework"
+              )
+              .flatMap((entry) => path.join(entry.parentPath, entry.name));
+          }
+        } else {
+          // Traverse into the child directory
+          return findFrameworkPaths(path.join(currentPath, file.name));
+        }
       }
       return [];
     });
@@ -97,105 +138,6 @@ export function updateInfoPlist({
   fs.writeFileSync(filePath, updatedContents, "utf-8");
 }
 
-export function rebuildXcframeworkHashed(modulePath: string) {
-  // Copy the xcframework to the output directory and rename the framework and binary
-  const hash = hashNodeApiModulePath(modulePath);
-  const tempPath = path.join(XCFRAMEWORKS_PATH, `node-api-${hash}-temp`);
-  try {
-    const outputPath = path.join(
-      XCFRAMEWORKS_PATH,
-      `node-api-${hash}.xcframework`
-    );
-
-    fs.cpSync(modulePath, tempPath, { recursive: true });
-
-    const frameworkPaths = fs
-      .readdirSync(tempPath, {
-        withFileTypes: true,
-      })
-      .flatMap((tripletEntry) => {
-        if (tripletEntry.isDirectory()) {
-          const tripletPath = path.join(tempPath, tripletEntry.name);
-          return fs
-            .readdirSync(tripletPath, {
-              withFileTypes: true,
-            })
-            .flatMap((frameworkEntry) => {
-              if (
-                frameworkEntry.isDirectory() &&
-                path.extname(frameworkEntry.name) === ".framework"
-              ) {
-                const frameworkPath = path.join(
-                  tripletPath,
-                  frameworkEntry.name
-                );
-                const oldLibraryName = path.basename(
-                  frameworkEntry.name,
-                  ".framework"
-                );
-                const oldLibraryPath = path.join(frameworkPath, oldLibraryName);
-                const newLibraryName = `node-api-${hash}`;
-                const newFrameworkPath = path.join(
-                  tripletPath,
-                  `${newLibraryName}.framework`
-                );
-                const newLibraryPath = path.join(
-                  newFrameworkPath,
-                  newLibraryName
-                );
-                assert(
-                  fs.existsSync(oldLibraryPath),
-                  `Expected a library at '${oldLibraryPath}'`
-                );
-                // Rename the library
-                fs.renameSync(
-                  oldLibraryPath,
-                  // Cannot use newLibraryPath here, because the framework isn't renamed yet
-                  path.join(frameworkPath, `node-api-${hash}`)
-                );
-                // Rename the framework
-                fs.renameSync(frameworkPath, newFrameworkPath);
-                // Expect the library in the new location
-                assert(fs.existsSync(newLibraryPath));
-                // Update the binary
-                cp.spawnSync("install_name_tool", [
-                  "-id",
-                  `@rpath/${newLibraryName}.framework/${newLibraryName}`,
-                  newLibraryPath,
-                ]);
-                // Update the Info.plist file for the framework
-                updateInfoPlist({
-                  filePath: path.join(newFrameworkPath, "Info.plist"),
-                  oldLibraryName,
-                  newLibraryName,
-                });
-                return newFrameworkPath;
-              } else {
-                return [];
-              }
-            });
-        } else {
-          return [];
-        }
-      });
-
-    // Create a new xcframework from the renamed frameworks
-    cp.spawnSync("xcodebuild", [
-      "-create-xcframework",
-      ...frameworkPaths.flatMap((frameworkPath) => [
-        "-framework",
-        frameworkPath,
-      ]),
-      "-output",
-      outputPath,
-    ]);
-
-    return outputPath;
-  } finally {
-    fs.rmSync(tempPath, { recursive: true, force: true });
-  }
-}
-
 export const program = new Command("react-native-node-api-modules");
 
 program
@@ -204,43 +146,93 @@ program
     path.resolve(process.cwd(), p)
   )
   .action((installationRoot: string) => {
-    // Find the location of each dependency
-    const dependencyPathsByName = findPackageDependencyPaths(installationRoot);
-    // Find all their xcframeworks
-    const dependenciesByName = Object.fromEntries(
-      Object.entries(dependencyPathsByName)
-        .map(([dependencyName, dependencyPath]) => {
-          // Make all the xcframeworks relative to the dependency path
-          const xcframeworkPaths = findXCFrameworkPaths(dependencyPath).map(
-            (p) => path.relative(dependencyPath, p)
-          );
-          return [
-            dependencyName,
-            {
-              path: dependencyPath,
-              xcframeworkPaths,
-            },
-          ] as const;
-        })
-        // Remove any dependencies without xcframeworks
-        .filter(([, { xcframeworkPaths }]) => xcframeworkPaths.length > 0)
+    const tempPath = fs.mkdtempSync(
+      path.join(os.tmpdir(), "react-native-node-api-frameworks-")
     );
-    // To be able to reference the xcframeworks from the Podspec,
-    // we need them as sub-directories of the Podspec parent directory.
-    // Create or clean the output directory
-    fs.rmSync(XCFRAMEWORKS_PATH, { recursive: true, force: true });
-    fs.mkdirSync(XCFRAMEWORKS_PATH, { recursive: true });
-    // Create symbolic links for each xcframework found in dependencies
-    const xcframeworkPaths = Object.entries(dependenciesByName).flatMap(
-      ([, dependency]) => {
-        return dependency.xcframeworkPaths.map((xcframeworkPath) => {
-          return rebuildXcframeworkHashed(
-            path.join(dependency.path, xcframeworkPath)
-          );
+    const pathsToCleanup = [tempPath];
+    try {
+      // Find the location of each dependency
+      const dependencyPathsByName =
+        findPackageDependencyPaths(installationRoot);
+      // Find all their xcframeworks
+      const frameworkPaths = Object.entries(dependencyPathsByName).flatMap(
+        ([, dependencyPath]) => findFrameworkPaths(dependencyPath)
+      );
+
+      // To be able to reference the xcframeworks from the Podspec,
+      // we need them as sub-directories of the Podspec parent directory.
+
+      // Create or clean the output directory
+      fs.rmSync(XCFRAMEWORK_PATH, { recursive: true, force: true });
+
+      const renamedFrameworkPaths = frameworkPaths.map((frameworkPath) => {
+        const modulePath = path.resolve(frameworkPath, "../..");
+        assert(path.extname(modulePath) === ".xcframework");
+        const hash = hashNodeApiModulePath(modulePath);
+
+        const tempFrameworkPath = fs.mkdtempSync(
+          path.join(os.tmpdir(), `react-native-node-api-framework-${hash}-`)
+        );
+        pathsToCleanup.push(tempFrameworkPath);
+
+        // Copy the framework and library (renaming the framework in the process)
+        const oldLibraryName = path.basename(frameworkPath, ".framework");
+        // Expecting the binary to have the same name as the framework
+        assert(
+          fs.existsSync(path.join(frameworkPath, oldLibraryName)),
+          `Expected a library binary named '${oldLibraryName}' in the framework '${frameworkPath}'`
+        );
+        const newLibraryName = `node-api-${hash}`;
+        const newFrameworkPath = path.join(
+          tempFrameworkPath,
+          `${newLibraryName}.framework`
+        );
+        const newLibraryPath = path.join(newFrameworkPath, newLibraryName);
+        fs.cpSync(frameworkPath, newFrameworkPath, { recursive: true });
+        // Rename the library file
+        fs.renameSync(
+          path.join(newFrameworkPath, oldLibraryName),
+          newLibraryPath
+        );
+        // Update the binary
+        cp.spawnSync("install_name_tool", [
+          "-id",
+          `@rpath/${newLibraryName}.framework/${newLibraryName}`,
+          newLibraryPath,
+        ]);
+        // Update the Info.plist file for the framework
+        updateInfoPlist({
+          filePath: path.join(newFrameworkPath, "Info.plist"),
+          oldLibraryName,
+          newLibraryName,
         });
+
+        return newFrameworkPath;
+      });
+
+      // Combine all frameworks into a single xcframework
+      const { status } = cp.spawnSync(
+        "xcodebuild",
+        [
+          "-create-xcframework",
+          ...renamedFrameworkPaths.flatMap((frameworkPath) => [
+            "-framework",
+            frameworkPath,
+          ]),
+          "-output",
+          XCFRAMEWORK_PATH,
+        ],
+        {
+          stdio: "inherit",
+        }
+      );
+      assert.equal(status, 0, "Failed building xcframework");
+      console.log("Wrote xcframework to", XCFRAMEWORK_PATH);
+    } finally {
+      for (const pathToCleanup of pathsToCleanup) {
+        fs.rmSync(pathToCleanup, { recursive: true, force: true });
       }
-    );
-    console.log(JSON.stringify(xcframeworkPaths, null, 2));
+    }
   });
 
 export function run(argv: string[]) {
