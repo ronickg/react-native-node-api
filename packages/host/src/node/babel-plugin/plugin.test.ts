824c9325d24ff53faea7d6476bc90fa2adca8cdf
Mariusz Pasinski <mariusz.pasinski@callstack.com>
2025-06-11 17:58:07 +0200

    feat: run each test case as separate call to `it()`

--- packages/host/src/node/babel-plugin/plugin.test.ts
+++ packages/host/src/node/babel-plugin/plugin.test.ts
@@ -8,79 +8,79 @@ import { plugin, findNodeAddonForBindings, type PluginOptions } from "./plugin.j
 import { setupTempDirectory } from "../test-utils.js";
 
 describe("plugin", () => {
-  it("transforms require calls, regardless", (context) => {
-    const tempDirectoryPath = setupTempDirectory(context, {
-      "package.json": `{ "name": "my-package" }`,
-      "addon-1.node":
-        "// This is supposed to be a binary file",
-      "addon-2.node":
-        "// This is supposed to be a binary file",
-      "addon-1.js": `
-        const addon = require('./addon-1.node');
-        console.log(addon);
-      `,
-      "addon-2.js": `
-        const addon = require('./addon-2.node');
-        console.log(addon);
-      `,
-      "sub-directory/addon-1.js": `
-        const addon = require('../addon-1.node');
-        console.log(addon);
-      `,
-      "sub-directory-3/package.json": `{ "name": "sub-package" }`,
-      "sub-directory-3/addon-outside.js": `
-        const addon = require('../addon-2.node');
-        console.log(addon);
-      `,
-      "addon-1-bindings.js": `
-        const addon = require('bindings')('addon-1');
-        console.log(addon);
-      `,
-      "require-js-file.js": `
-        const addon = require('./addon-1.js');
-        console.log(addon);
-      `,
-    });
-
+  describe("transforms require calls, regardless", () => {
     const EXPECTED_PKG_NAME = "my-package";
 
     type TestCaseParams = {
       resolvedPath?: string;
       originalPath: string;
       inputFile: string;
-      options?: PluginOptions;
-    };
-    const runTestCase = ({
-      resolvedPath,
-      originalPath,
-      inputFile,
-      options,
-    }: TestCaseParams) => {
-      const result = transformFileSync(
-        path.join(tempDirectoryPath, inputFile),
-        { plugins: [[plugin, options]] }
-      );
-      assert(result);
-      const { code } = result;
-      if (!resolvedPath) {
-        assert(
-            code && !code.includes(`requireNodeAddon`),
-            `Unexpected code: ${code}`
-        );
-      } else {
-        assert(
-            code && code.includes(`requireNodeAddon("${resolvedPath}", "${EXPECTED_PKG_NAME}", "${originalPath}")`),
-            `Unexpected code: ${code}`
-        );
-      }
     };
 
-    runTestCase({ resolvedPath: "./addon-1.node", originalPath: "./addon-1.node", inputFile: "./addon-1.js" });
-    runTestCase({ resolvedPath: "./addon-2.node", originalPath: "./addon-2.node", inputFile: "./addon-2.js" });
-    runTestCase({ resolvedPath: "./addon-1.node", originalPath: "../addon-1.node", inputFile: "./sub-directory/addon-1.js" });
-    runTestCase({ resolvedPath: "./addon-2.node", originalPath: "../addon-2.node", inputFile: "./sub-directory-3/addon-outside.js" });
-    runTestCase({ resolvedPath: "./addon-1.node", originalPath: "addon-1", inputFile: "./addon-1-bindings.js" });
-    runTestCase({ resolvedPath: undefined, originalPath: "./addon-1.js", inputFile: "./require-js-file.js" });
+    ([
+      { resolvedPath: "./addon-1.node", originalPath: "./addon-1.node", inputFile: "./addon-1.js" },
+      { resolvedPath: "./addon-2.node", originalPath: "./addon-2.node", inputFile: "./addon-2.js" },
+      { resolvedPath: "./addon-1.node", originalPath: "../addon-1.node", inputFile: "./sub-directory/addon-1.js" },
+      { resolvedPath: "./addon-2.node", originalPath: "../addon-2.node", inputFile: "./sub-directory-3/addon-outside.js" },
+      { resolvedPath: "./addon-1.node", originalPath: "addon-1", inputFile: "./addon-1-bindings.js" },
+      { resolvedPath: undefined, originalPath: "./addon-1.js", inputFile: "./require-js-file.js" },
+    ] as TestCaseParams[]).forEach(({ resolvedPath, originalPath, inputFile }) => {
+      const expectedMessage = resolvedPath
+        ? `transform to requireNodeAddon() with "${resolvedPath}"`
+        : "NOT transform to requireNodeAddon()";
+
+      it(`${inputFile} should ${expectedMessage}`, (context) => {
+        const tempDirectoryPath = setupTempDirectory(context, {
+          "package.json": `{ "name": "${EXPECTED_PKG_NAME}" }`,
+          "addon-1.node":
+              "// This is supposed to be a binary file",
+          "addon-2.node":
+              "// This is supposed to be a binary file",
+          "addon-1.js": `
+            const addon = require('./addon-1.node');
+            console.log(addon);
+          `,
+          "addon-2.js": `
+            const addon = require('./addon-2.node');
+            console.log(addon);
+          `,
+          "sub-directory/addon-1.js": `
+            const addon = require('../addon-1.node');
+            console.log(addon);
+          `,
+          "sub-directory-3/package.json": `{ "name": "sub-package" }`,
+          "sub-directory-3/addon-outside.js": `
+            const addon = require('../addon-2.node');
+            console.log(addon);
+          `,
+          "addon-1-bindings.js": `
+            const addon = require('bindings')('addon-1');
+            console.log(addon);
+          `,
+          "require-js-file.js": `
+            const addon = require('./addon-1.js');
+            console.log(addon);
+          `,
+        });
+        const result = transformFileSync(
+          path.join(tempDirectoryPath, inputFile),
+          { plugins: [[plugin, {}]] }
+        );
+        assert(result);
+        const { code } = result;
+        if (!resolvedPath) {
+          assert(
+              code && !code.includes(`requireNodeAddon`),
+              `Unexpected code: ${code}`
+          );
+        } else {
+          assert(
+              code && code.includes(`requireNodeAddon("${resolvedPath}", "${EXPECTED_PKG_NAME}", "${originalPath}")`),
+              `Unexpected code: ${code}`
+          );
+        }
+      });
+    });
   });
 
   it("transforms require calls to packages with native entry point", (context) => {
